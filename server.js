fad39662fe139117fadc89d6db0bcdb105ae2ee1
joao pedro <joaop.pateis07@gmail.com>
2026-02-10 14:45:39 -0300

    commit 2

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/ping', (req, res) => {
 
 // Rota fallback: index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
