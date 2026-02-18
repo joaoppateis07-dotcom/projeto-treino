30271c8ff6cdda76628c0e097b3cd521b3edc560
joao pedro <joaop.pateis07@gmail.com>
2026-02-18 10:34:45 -0300

    ir para funcionario

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-const btnIrParaFuncionario = document.getElementById('btnIrParaFuncionario');
-const btnVoltarDashboard = document.getElementById('btnVoltarDashboard');
-const sectionDashboard = document.getElementById('tela-dashboard');
-const sectionFuncionario = document.getElementById('tela-funcionario');
-
-btnIrParaFuncionario.addEventListener('click', () => {
-    sectionDashboard.classList.add('hidden');
-    sectionFuncionario.classList.remove('hidden');
-});
-
-btnVoltarDashboard.addEventListener('click', () => {
-    sectionFuncionario.classList.add('hidden');
-    sectionDashboard.classList.remove('hidden');
-});
 const btnNovaPasta = document.getElementById('btnNovaPasta');
 const modalNovaPasta = document.getElementById('modalNovaPasta')
 const btnCancelarPasta = document.getElementById('btnCancelarPasta');
