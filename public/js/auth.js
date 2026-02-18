12a1eb506149e7662b23d2e760ce5db075f3fd9f
joao pedro <joaop.pateis07@gmail.com>
2026-02-18 11:21:34 -0300

    integração

--- public/js/auth.js
+++
@@ -1,6 +0,0 @@
-// Verificação adicional no cliente: limpa qualquer dados locais
-// (A proteção REAL é no servidor via cookie autenticado)
-(function() {
-  localStorage.removeItem('logado');
-  localStorage.removeItem('savedCredentials');
-})();
